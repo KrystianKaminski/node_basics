ff809025fe628f38aa9b93f9c348dfea2a6f0cc0
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-30 15:22:43 +0100

    Write hello HTTP on localhost:3000

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const http = require("http");
 
 const server = http.createServer();
+
+// events
+
+server.on("request", (req, res) => {
+  // do
+  console.log("Request!!!");
+  res.write("Hello HTTP");
+  res.end();
+});
+
 server.listen(3000);
